6918e1b6da68f1e2b7576fecb0f1665bd093e14a
Noah <39002725+noaahh@users.noreply.github.com>
2026-03-12 22:15:46 +0100

    disable zsh-autosuggestions

--- dot_zsh.d/plugins.sh
+++ dot_zsh.d/plugins.sh
@@ -1,9 +1,7 @@
-#!/bin/zsh
-
 # Created by Zap Installer
 [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
 
-plug "zsh-users/zsh-autosuggestions"
+# plug "zsh-users/zsh-autosuggestions"
 plug "zap-zsh/supercharge"
 plug "zap-zsh/zap-prompt"
 plug "wintermi/zsh-brew"

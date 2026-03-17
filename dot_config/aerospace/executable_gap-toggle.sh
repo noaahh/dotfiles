5dff0980bbb17d4bf1d285e002beadb817af4530
Noah <39002725+noaahh@users.noreply.github.com>
2026-03-17 10:45:35 +0100

    update aerospace config

---
+++ dot_config/aerospace/executable_gap-toggle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CONFIG="$HOME/.config/aerospace/aerospace.toml"
+
+MONITORS=$(aerospace list-monitors --json 2>/dev/null)
+MONITOR_COUNT=$(echo "$MONITORS" | grep -c '"monitor-name"')
+HAS_BUILTIN=$(echo "$MONITORS" | grep -ci "built-in")
+
+if [ "$MONITOR_COUNT" -eq 1 ] && [ "$HAS_BUILTIN" -gt 0 ]; then
+  sed -i '' 's/outer.top = [0-9]*/outer.top = 0/' "$CONFIG"
+else
+  sed -i '' 's/outer.top = [0-9]*/outer.top = 33/' "$CONFIG"
+fi
+
+aerospace reload-config

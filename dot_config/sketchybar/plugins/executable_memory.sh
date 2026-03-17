53bd34fe466732a877954dacb0b509e302b413f0
Noah <39002725+noaahh@users.noreply.github.com>
2026-03-17 09:59:12 +0100

    update sketchybar config

--- dot_config/sketchybar/plugins/executable_memory.sh
+++ dot_config/sketchybar/plugins/executable_memory.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-MEMORY=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{print 100-$5}' | tr -d '%')
-sketchybar --set $NAME icon.drawing=off label="mem ${MEMORY}%"
+TOTAL_MEM=$(sysctl -n hw.memsize)
+TOTAL_GB=$(echo "scale=0; $TOTAL_MEM / 1073741824" | bc)
+
+PAGE_SIZE=$(sysctl -n hw.pagesize)
+VM_STAT=$(vm_stat)
+PAGES_FREE=$(echo "$VM_STAT" | awk '/Pages free/ {gsub(/\./,"",$3); print $3}')
+PAGES_INACTIVE=$(echo "$VM_STAT" | awk '/Pages inactive/ {gsub(/\./,"",$3); print $3}')
+PAGES_SPECULATIVE=$(echo "$VM_STAT" | awk '/Pages speculative/ {gsub(/\./,"",$3); print $3}')
+PAGES_PURGEABLE=$(echo "$VM_STAT" | awk '/Pages purgeable/ {gsub(/\./,"",$3); print $3}')
+
+FREE_BYTES=$(( (PAGES_FREE + PAGES_INACTIVE + PAGES_SPECULATIVE + PAGES_PURGEABLE) * PAGE_SIZE ))
+USED_BYTES=$(( TOTAL_MEM - FREE_BYTES ))
+USED_GB=$(echo "scale=1; $USED_BYTES / 1073741824" | bc)
+PCT=$(echo "scale=0; $USED_BYTES * 100 / $TOTAL_MEM" | bc)
+
+sketchybar --set $NAME icon.drawing=off label="mem ${USED_GB}/${TOTAL_GB}G ${PCT}%"

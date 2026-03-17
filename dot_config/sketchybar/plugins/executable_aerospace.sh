bd37dcfbd5905fbc1369cf301fea8c1ae607cd05
Noah <39002725+noaahh@users.noreply.github.com>
2026-03-17 11:03:38 +0100

    minimal text workspace indicators

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Highlight the active aerospace workspace
 if [ "$1" = "$FOCUSED_WORKSPACE" ] || [ "$1" = "$(aerospace list-workspaces --focused)" ]; then
-  sketchybar --set $NAME background.color=0xff7aa2f7 \
-                         icon.color=0xff1e1e2e
+  sketchybar --set $NAME label.color=0xffffffff
 else
-  sketchybar --set $NAME background.color=0x00000000 \
-                         icon.color=0xffcad3f5
+  sketchybar --set $NAME label.color=0xff555555
 fi

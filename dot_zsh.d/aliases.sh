6f8a692fa28de637c4d3ac704207257f05679faa
Noah <39002725+noaahh@users.noreply.github.com>
2026-04-14 08:58:18 +0200

    remove todo.sh aliases

--- dot_zsh.d/aliases.sh
+++ dot_zsh.d/aliases.sh
@@ -9,11 +9,3 @@ alias sed="gsed"
 alias awk="gawk"
 
 
-# TODO aliases
-alias todo='todo.sh'
-alias t='todo.sh'
-alias td='todo.sh done'
-alias ta='todo.sh add'
-alias tl='todo.sh list'
-alias tp='todo.sh pri'
-alias te='nvim ~/DB/todo.txt' # let's me just take a look at the todo file
